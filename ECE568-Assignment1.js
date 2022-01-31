cc9eb38152874c0bab6e6d9e5c8bfcf63aaa5c90
Gagan G M <30140695+gagangm@users.noreply.github.com>
2022-01-31 06:33:52 +0530

    ECE568 SWA - Assignment 1
    Assignment 1 Code

---
+++ ECE568-Assignment1.js
@@ -0,0 +1,9 @@
+const data = [{a: 1}, {b: 2}, {a: 3}];
+
+let pluck = data.map((item) => { 
+  if (item.hasOwnProperty('a')) {
+    return item.a;
+  }
+}).filter(notUndefined => notUndefined !== undefined);
+
+console.log(pluck);
