3a7469d39c1a400e52e57d20f7b3231bee874273
Gagan G M <30140695+gagangm@users.noreply.github.com>
2022-02-01 08:56:15 +0530

    Update ECE568-Assignment1.js

--- ECE568-Assignment1.js
+++ ECE568-Assignment1.js
@@ -1,9 +1,19 @@
 const data = [{a: 1}, {b: 2}, {a: 3}];
+const propName = 'a';
 
-const pluck = data.map((item) => { 
-  if (item.hasOwnProperty('a')) {
-    return item.a;
-  }
-}).filter(notUndefined => notUndefined !== undefined);
+// const pluck = data.map((item) => { 
+//   if (item.hasOwnProperty('a')) {
+//     return item.a;
+//   }
+// }).filter(notUndefined => notUndefined !== undefined);
 
-console.log(pluck);
+const pluck = (propName) => {
+  const value = data.map((item) => { 
+    if (item.hasOwnProperty(propName)) {
+      return item.a;
+    }
+  }).filter(notUndefined => notUndefined !== undefined);
+  return value;
+};
+
+console.log(pluck(propName));
